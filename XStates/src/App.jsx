e6ad5bf8bc7c3f2578d582644fdfcef998f9f54b
Allan Kamson <allan.kamson0209@gmail.com>
2025-06-06 00:21:07 +0530

    Updated XStates

---
+++ XStates/src/App.jsx
@@ -0,0 +1,15 @@
+import React, { useState, useEffect } from 'react';
+import CitySelector from './components/CitySelector';
+
+
+function App() {
+  //const [count, setCount] = useState(0)
+
+  return (
+      <div>
+        <CitySelector />
+      </div>
+  );
+}
+
+export default App;
